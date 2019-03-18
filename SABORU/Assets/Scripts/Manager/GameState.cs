d99466d85f33565441a03914d1a3ca43d50fb121
ishidashota <c611621050@tokushima-u.ac.jp>
2019-03-18 15:33:52 +0900

    現在のゲーム状態を管理するクラスを追加

---
+++ SABORU/Assets/Scripts/Manager/GameState.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState : MonoBehaviour
+{
+
+    // 現在のゲーム進行状態
+    public enum Progress {
+        None = 1,
+        Title,
+        Playing,
+        GameOver,
+        Clear,
+        // TODO 分岐するならこれより下にも追加する
+        
+    }
+
+}
