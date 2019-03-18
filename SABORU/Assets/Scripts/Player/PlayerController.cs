bee59b80cf7e3c3c2eb0d13cda98caf3d5a23cc3
honda-tsuyoshi <gangben@gmail.com>
2019-03-18 18:21:14 +0900

    PlayerControllerに不具合があったので修正

--- SABORU/Assets/Scripts/Player/PlayerController.cs
+++ SABORU/Assets/Scripts/Player/PlayerController.cs
@@ -22,33 +22,46 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //状態がsittingのときの状態遷移
-        if (playerState == PlayerState.Sitting)
-            {
+        switch (playerState)
+        {
+            case PlayerState.Sitting:
                 if (Input.GetKeyDown(KeyCode.Space))
                 {
                     StateChanged(PlayerState.Standing);
                     Debug.Log("Standing");
                 }
-                if (Input.GetKeyDown(KeyCode.KeypadEnter))
+                if (Input.GetKeyDown(KeyCode.Return))
                 {
                     StateChanged(PlayerState.Tiping);
                     Debug.Log("Tiping");
                 }
-            }
-        //状態がstandingのときの状態遷移
-        else if (playerState == PlayerState.Standing)
-        {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                StateChanged(PlayerState.Sitting);
-                Debug.Log("Sitting");
-            }
-            if (Input.GetKeyDown(KeyCode.KeypadEnter))
-            {
-                StateChanged(PlayerState.Resting);
-                Debug.Log("Resting");
-            }
+                break;
+            case PlayerState.Standing:
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    StateChanged(PlayerState.Sitting);
+                    Debug.Log("Sitting");
+                }
+                if (Input.GetKeyDown(KeyCode.Return))
+                {
+                    StateChanged(PlayerState.Resting);
+                    Debug.Log("Resting");
+                }
+                break;
+            case PlayerState.Tiping:
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    StateChanged(PlayerState.Sitting);
+                    Debug.Log("Sitting");
+                }
+                break;
+            case PlayerState.Resting:
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    StateChanged(PlayerState.Standing);
+                    Debug.Log("Standing");
+                }
+                break;
         }
         
     }
@@ -71,6 +84,7 @@ public class PlayerController : MonoBehaviour
                 break;
             case PlayerState.Resting:
                 playerState = PlayerState.Resting;
+                playerMove.enabled = false;
                 /* Restingスクリプト呼び出し */
                 break;
         }
