f9579c33e77c1a18d0fa11600ffc002cb08bfe3f
Kawakami-Akua <kawakame2453@gmail.com>
2019-03-19 21:06:18 +0900

    UI関係作りました

---
+++ SABORU/Assets/Scripts/UI/PauseManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject pauseUI;
+
+    void Start(){
+        pauseUI.SetActive(false);
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)){
+
+            pauseUI.SetActive(!pauseUI.activeSelf);
+
+            if(pauseUI.activeSelf){
+                Time.timeScale = 0f;
+        }else{
+            Time.timeScale = 1f;
+        }
+    }   
+    }
+
+}
---
+++ SABORU/Assets/Scripts/UI/Quit_Game.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quit_Game : MonoBehaviour
+{
+
+    // Update is called once per frame
+    public　void OnClick()
+    {
+        Application.Quit();
+    }
+}
