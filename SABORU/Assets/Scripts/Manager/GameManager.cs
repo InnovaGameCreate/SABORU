d2383b6ec088b1ab76087c018aad1a8a7e8937b8
ishidashota <c611621050@tokushima-u.ac.jp>
2019-03-19 16:59:32 +0900

    シーン遷移ロジック完成

--- SABORU/Assets/Scripts/Manager/GameManager.cs
+++ SABORU/Assets/Scripts/Manager/GameManager.cs
@@ -1,18 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using System;
 
 
 public class GameManager : Singleton<GameManager>
 {
     // Start is called before the first frame update
-
     [SerializeField]
     private GameState.Progress nowState = GameState.Progress.Title;
     private GameState.Progress prevState = GameState.Progress.None;
 
-    public event Func<GameState.Progress> OnChangeGameState;
+    public bool isGameOver = false;
+
+    public event Action OnChangeGameState;
 
     /// <summary>
     /// 現状の進行状態
@@ -24,7 +26,11 @@ public class GameManager : Singleton<GameManager>
         set { 
             prevState = nowState;
             nowState = value;
-
+            
+            var sceneName = Enum.GetName(typeof(GameState.Progress), nowState );
+            if(nowState != GameState.Progress.None)
+                SceneManager.LoadScene(sceneName);
+            Debug.Log("[GameManager] Now scene is " + sceneName);
             // 状態が変化したことを通知する
             OnChangeGameState(); 
         }
---
+++ SABORU/Assets/Scripts/Scenes/Title.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Title : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject gameStartView;
+    [SerializeField]
+    private string nextSceneName;
+    private Button startButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startButton = gameStartView.GetComponent<Button>();
+
+        // スタートボタンを押した時のイベント処理を登録
+        startButton.onClick.AddListener(()=> {
+            // SceneManager.LoadScene(nextSceneName);
+            GameManager.Instance.NowState = GameState.Progress.Main;
+        });
+
+        // ゲームシーンが変更された事を登録
+        GameManager.Instance.OnChangeGameState += () => {
+        
+        };
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
