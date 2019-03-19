cdf4ccb5083ba9a9acd06ddb9336152fd5d1d977
ryota100825 <c611701465@tokushima-u.ac.jp>
2019-03-19 20:25:35 +0900

    MainにStage追加

--- SABORU/Assets/Scripts/Player/Player.cs
+++ SABORU/Assets/Scripts/Player/Player.cs
@@ -7,10 +7,8 @@ public class Player : MonoBehaviour
 {
     //playerのダメージ
     public float playerDamagePoint = 0;
-    //稼いだお金
-    public int getMoney = 0;
-    //所持金を表示するテキスト用変数
-    public Text getMoneyText;
+
+    public GameObject Vender;
     //スライダーコンポーネント格納変数
     Slider slider;
 
@@ -23,14 +21,32 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        playerDamagePoint+=1;
+        playerDamagePoint+=0.5f;
         if(playerDamagePoint > slider.maxValue){
             playerDamagePoint = slider.minValue;
         }
 
         slider.value = playerDamagePoint;
 
-        getMoneyText.text = "所持金：" + getMoney.ToString() + "円";
+
         
     }
+
+    void OnTriggerStay(Collider col)
+    {
+        if (col.gameObject.tag == "vender")
+        {
+            Debug.Log("あたり");
+            Vender.SetActive(true);
+            playerDamagePoint -= 1.0f;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        Vender.SetActive(false);
+
+    }
+
+
 }
--- SABORU/Assets/Scripts/Player/PlayerController.cs
+++ SABORU/Assets/Scripts/Player/PlayerController.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
     public enum PlayerState
     {
-        Sitting, Standing, Tiping, Resting
+        Sitting, Standing
     }
 
 
@@ -12,6 +13,11 @@ public class PlayerController : MonoBehaviour
 {
     public PlayerMove playerMove;
     public PlayerState playerState;
+    public GameObject Task;
+    //稼いだお金
+    public int getMoney = 0;
+    //所持金を表示するテキスト用変数
+    public Text getMoneys;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,52 +25,6 @@ public class PlayerController : MonoBehaviour
         playerMove = GetComponent<PlayerMove>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        switch (playerState)
-        {
-            case PlayerState.Sitting:
-                if (Input.GetKeyDown(KeyCode.Space))
-                {
-                    StateChanged(PlayerState.Standing);
-                    Debug.Log("Standing");
-                }
-                if (Input.GetKeyDown(KeyCode.Return))
-                {
-                    StateChanged(PlayerState.Tiping);
-                    Debug.Log("Tiping");
-                }
-                break;
-            case PlayerState.Standing:
-                if (Input.GetKeyDown(KeyCode.Space))
-                {
-                    StateChanged(PlayerState.Sitting);
-                    Debug.Log("Sitting");
-                }
-                if (Input.GetKeyDown(KeyCode.Return))
-                {
-                    StateChanged(PlayerState.Resting);
-                    Debug.Log("Resting");
-                }
-                break;
-            case PlayerState.Tiping:
-                if (Input.GetKeyDown(KeyCode.Space))
-                {
-                    StateChanged(PlayerState.Sitting);
-                    Debug.Log("Sitting");
-                }
-                break;
-            case PlayerState.Resting:
-                if (Input.GetKeyDown(KeyCode.Space))
-                {
-                    StateChanged(PlayerState.Standing);
-                    Debug.Log("Standing");
-                }
-                break;
-        }
-        
-    }
     //状態が変化するときの処理を行う
     void StateChanged(PlayerState p)
     {
@@ -78,16 +38,49 @@ public class PlayerController : MonoBehaviour
                 playerState = PlayerState.Standing;
                 playerMove.enabled = true;
                 break;
-            case PlayerState.Tiping:
-                playerState = PlayerState.Tiping;
-                /* Tipingスクリプト呼び出し */
-                break;
-            case PlayerState.Resting:
-                playerState = PlayerState.Resting;
-                playerMove.enabled = false;
-                /* Restingスクリプト呼び出し */
-                break;
         }
     }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (playerState)
+        {
+            case PlayerState.Sitting:
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    StateChanged(PlayerState.Standing);
+                    Debug.Log("Standing");
+                }
+                break;
+            case PlayerState.Standing:
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    StateChanged(PlayerState.Sitting);
+                    Debug.Log("Sitting");
+                }
+                break;
+        }
+
+        getMoneys.text = "所持金：" + getMoney.ToString() + "円";
+        
+    }
+
+    void OnTriggerStay(Collider col)
+    {
+        if (col.gameObject.tag == "Chair" && playerState == PlayerState.Sitting)
+        {
+            Debug.Log("あたり");
+            Task.SetActive(true);
+            getMoney = getMoney + 10;
+            }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        Task.SetActive(false);
+
+    }
+
 
 }
